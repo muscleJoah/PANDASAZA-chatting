d10b6b6aa141bc737b4c582c97aa665792c0093a
muscleJoah <wwlwwowwn12@gmail.com>
2021-03-18 00:23:27 +0900

    TODO : FIX CORS Error

---
+++ src/main/java/com/websocket/chat/config/CssConfig.java
@@ -0,0 +1,35 @@
+package com.websocket.chat.config;
+
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+
+@Configuration
+public class CssConfig extends WebMvcConfigurationSupport {
+
+
+
+    // Solve No mapping for GET /webjars/bootstrap
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations(
+                    "classpath:/META-INF/resources/webjars/");
+        }
+        if (!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(
+                    CLASSPATH_RESOURCE_LOCATIONS);
+        }
+
+    }
+
+}
--- src/main/java/com/websocket/chat/config/MvcConfiguration.java
+++ src/main/java/com/websocket/chat/config/MvcConfiguration.java
@@ -3,6 +3,7 @@ package com.websocket.chat.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceView;
@@ -19,4 +20,6 @@ public class MvcConfiguration implements WebMvcConfigurer {
 
         return resolver;
     }
+
+
 }
