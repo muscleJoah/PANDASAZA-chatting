fdacd732b000d54755df1f9badd4f76719bce3c7
muscleJoah <wwlwwowwn12@gmail.com>
2021-03-29 22:54:57 +0900

    FIX ERROR

---
+++ src/main/java/com/websocket/chat/config/EmbeddedRedisConfig.java
@@ -0,0 +1,37 @@
+package com.websocket.chat.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Profile("local")
+@Configuration
+public class EmbeddedRedisConfig {
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() {
+        redisServer = RedisServer.builder().port(redisPort).setting("maxheap 128M").setting("daemonize no").setting("appendonly no").build();
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+
+
+
+}
